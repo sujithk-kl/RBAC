c0f010ba0c2d2b3c8184ab723e4ce7222bf4d884
sujith-kl <sujithkumaravel03@gmail.com.com>
2024-12-21 10:52:59 +0530

    f updates

---
+++ frontend/src/pages/CEOPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CEOPage = () => {
+  return (
+    <div>
+      <h1>CEO Dashboard</h1>
+      {/* Add components or logic for the CEO here */}
+    </div>
+  );
+};
+
+export default CEOPage;
---
+++ frontend/src/pages/ManagerPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ManagerPage = () => {
+  return (
+    <div>
+      <h1>Manager Dashboard</h1>
+      {/* Add components or logic for the Manager here */}
+    </div>
+  );
+};
+
+export default ManagerPage;
