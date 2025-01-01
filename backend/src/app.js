b22462d17465bb96d54afadc176b3e053151fb2e
davhsi <davish.std@gmail.com>
2025-01-01 11:19:53 +0530

    removed redundent code

--- backend/src/app.js
+++ backend/src/app.js
@@ -55,9 +55,6 @@ const pingWebsite = () => {
 pingWebsite();
 
 // Start the server (put this after everything is set up)
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
 
 module.exports = app;
